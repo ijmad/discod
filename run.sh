a03403d9eed59c4ca026ebc98831e1226bc20844
Ian Maddison <ian@i.need.stronger.coffee>
2015-08-17 23:51:36 +0100

    Forward docker socket for unix socket scenarios

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ RES_ID=$(docker run -d \
  -e "DOCKER_TLS_VERIFY=$DOCKER_TLS_VERIFY" \
  -e "DOCKER_CERT_PATH=/cert" \
  -v $DOCKER_CERT_PATH:/cert \
+ -v /var/run/docker.sock:/var/run/docker.sock \
  discod/service-resolver:latest)
 
 DNS_ID=$(docker run -d \
